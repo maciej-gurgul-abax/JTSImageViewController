63bba4dbba06f1b5fd7cf3e226ab6b17ead98953
Maciej Gurgul <maciej.gurgul@abax.com>
2023-10-11 18:54:38 +0200

    remove test target

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "JTSImageViewController",
             dependencies: []),
-        .testTarget(
-            name: "JTSImageViewControllerTests",
-            dependencies: ["JTSImageViewController"]),
     ]
 )
